5f213cd47b602b769a7950668e1b25cd1339fc42
AI研究人 <y.takak1234@gmail.com>
2025-03-30 13:41:15 +0900

    main.py の作成

---
+++ main.py
@@ -0,0 +1,19 @@
+from gradio_client import Client
+
+print(Enter...)
+
+promptinput=input()
+
+client = Client("stabilityai/stable-diffusion-3.5-medium")
+result = client.predict(
+		prompt=promptinput,
+		negative_prompt="Hello!!",
+		seed=0,
+		randomize_seed=True,
+		width=1024,
+		height=1024,
+		guidance_scale=4.5,
+		num_inference_steps=40,
+		api_name="/infer"
+)
+print(result)
